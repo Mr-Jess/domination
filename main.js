0d0de86a8a0563828036731179d4fbf8192b92ba
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-10-15 12:25:17 -0400

    adding line through

--- main.js
+++ main.js
@@ -7,5 +7,8 @@ addImagineUrl('#image-1', 'https://f1.media.brightcove.com/8/1078702682/10787026
 addImagineUrl('#image-2', 'https://dw8stlw9qt0iz.cloudfront.net/G7E0aSNJuMsOodsc2glkjsR6Wt8=/fit-in/800x450/filters:format(jpeg):quality(75)/curiosity-data.s3.amazonaws.com/images/content/landscape/standard/f83f2ef0-098f-45a4-b14d-d9a39d9b53fa.jpg')
 addImagineUrl('#image-3', 'https://www.thoughtco.com/thmb/x6rKrEfzJsiP-wHxgfCEvAwLg4E=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/Irritator-58d292465f9b581d72de1f9c.jpg')
 
-
+function strikeThroughFirstItem() {
+    const firstItem = document.querySelector('ul#arguments > li;first-child');
+    firstItem.style.textDecoration = 'line-through';
+}
 
