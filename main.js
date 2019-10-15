69d745e29bfa57753c161d05fd2ba55c50b48420
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-10-15 12:21:36 -0400

    coding along with teachings

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-function imageSourceToAdd(id,url){
-    const newImage = document.querySelector(id)
-    newImage.src = url
+function addImagineUrl(id,url){
+    const image = document.querySelector(id)
+    image.src = url
 }
 
-const newUrl = 'https://f1.media.brightcove.com/8/1078702682/1078702682_6004950245001_6004956161001-vs.jpg?pubId=1078702682&videoId=6004956161001'
-imageSource('#image-1', newUrl);
-const newUrl2 = "https://images-na.ssl-images-amazon.com/images/I/71Ls63RJxeL._SL1500_.jpg"
-imageSource('#image-2', newUrl);
-const newUrl3 = "https://dw8stlw9qt0iz.cloudfront.net/G7E0aSNJuMsOodsc2glkjsR6Wt8=/fit-in/800x450/filters:format(jpeg):quality(75)/curiosity-data.s3.amazonaws.com/images/content/landscape/standard/f83f2ef0-098f-45a4-b14d-d9a39d9b53fa.jpg"
-imageSource('#image-3', newUrl);
+addImagineUrl('#image-1', 'https://f1.media.brightcove.com/8/1078702682/1078702682_6004950245001_6004956161001-vs.jpg?pubId=1078702682&videoId=6004956161001')
+addImagineUrl('#image-2', 'https://dw8stlw9qt0iz.cloudfront.net/G7E0aSNJuMsOodsc2glkjsR6Wt8=/fit-in/800x450/filters:format(jpeg):quality(75)/curiosity-data.s3.amazonaws.com/images/content/landscape/standard/f83f2ef0-098f-45a4-b14d-d9a39d9b53fa.jpg')
+addImagineUrl('#image-3', 'https://www.thoughtco.com/thmb/x6rKrEfzJsiP-wHxgfCEvAwLg4E=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/Irritator-58d292465f9b581d72de1f9c.jpg')
+
 
 
