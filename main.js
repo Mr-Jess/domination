b12a8fb2c918a959615383fee45328247aa279c6
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-10-15 12:16:54 -0400

    coding along with instructor

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ function imageSourceToAdd(id,url){
 }
 
 const newUrl = 'https://f1.media.brightcove.com/8/1078702682/1078702682_6004950245001_6004956161001-vs.jpg?pubId=1078702682&videoId=6004956161001'
-imageSource('#image-1',newUrl);
-const newUrl2 = ""
-imageSource('#image-2',newUrl);
-const newUrl3 = ""
-imageSource('#image-3',newUrl);
+imageSource('#image-1', newUrl);
+const newUrl2 = "https://images-na.ssl-images-amazon.com/images/I/71Ls63RJxeL._SL1500_.jpg"
+imageSource('#image-2', newUrl);
+const newUrl3 = "https://dw8stlw9qt0iz.cloudfront.net/G7E0aSNJuMsOodsc2glkjsR6Wt8=/fit-in/800x450/filters:format(jpeg):quality(75)/curiosity-data.s3.amazonaws.com/images/content/landscape/standard/f83f2ef0-098f-45a4-b14d-d9a39d9b53fa.jpg"
+imageSource('#image-3', newUrl);
+
+
