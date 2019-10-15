75abd8ff7e860637cf8e261999ec7fb772365557
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-10-15 14:07:14 -0400

    coding along

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//1
 function addImagineUrl(id,url){
     const image = document.querySelector(id)
     image.src = url
@@ -7,12 +8,14 @@ addImagineUrl('#image-1', 'https://f1.media.brightcove.com/8/1078702682/10787026
 addImagineUrl('#image-2', 'https://dw8stlw9qt0iz.cloudfront.net/G7E0aSNJuMsOodsc2glkjsR6Wt8=/fit-in/800x450/filters:format(jpeg):quality(75)/curiosity-data.s3.amazonaws.com/images/content/landscape/standard/f83f2ef0-098f-45a4-b14d-d9a39d9b53fa.jpg')
 addImagineUrl('#image-3', 'https://www.thoughtco.com/thmb/x6rKrEfzJsiP-wHxgfCEvAwLg4E=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/Irritator-58d292465f9b581d72de1f9c.jpg')
 
+//2
 function strikeThroughFirstItem() {
     const firstItem = document.querySelector('ul#arguments > li:first-child');
     firstItem.style.textDecoration = 'line-through';
 }
 strikeThroughFirstItem()
 
+//3
 function removeLastLi() {
     const lastLi = document.querySelector('ul > li:last-child');
     lastLi.remove();
@@ -23,21 +26,38 @@ function removeLastLi() {
 }
 removeLastLi();
 
+//4
 function addArgument(element) {
     //Quicker way = document.querySelector('ul#arguments')
     const arguments = document.querySelector('ul#arguments')
     arguments.appendChild(element);
 }
 
+//5
 const newImage = document.createElement('img');
 newImage.src = 'https://www.thoughtco.com/thmb/x6rKrEfzJsiP-wHxgfCEvAwLg4E=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/Irritator-58d292465f9b581d72de1f9c.jpg';
 newImage.style.height = '60px';
 
 addArgument(newImage);
 
+//6
 function makeSmall(image) {
-    image.style.height = '30px'
+    image.style.height = '30px';
 }
 
-const image1 = document.querySelector('#image-1')
-makeSmall(image1)
+const image1 = document.querySelector('#image-1');
+makeSmall(image1);
+
+//7
+function makeInvisible(element) {
+    element.className = 'invisible';
+}
+const disappearingTextFelid = document.querySelector('input');
+makeInvisible(disappearingTextFelid);
+
+//8
+function changeFontSize(element, fontSize) {
+    element.style.fontSize = 'fontSize';
+}
+const thing2 = document.querySelector('#thing-2')
+changeFontSize(thing2, '5px')
